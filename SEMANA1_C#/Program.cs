d959fcf28a22960db4eb00571ba3aa230c6220cf
analia <carrascoanalia992@gmail.com>
2025-09-05 10:13:40 -0500

    subiendo ejer2 sem3

--- SEMANA1_C#/Program.cs
+++ SEMANA1_C#/Program.cs
@@ -10,7 +10,7 @@ namespace SEMANA1_C_
     {
         static void Main(string[] args)
         {
-            ejer1();
+            ejer2();
             Console.ReadKey(); //realizando detenimiento de consola
         }
 
@@ -25,7 +25,9 @@ namespace SEMANA1_C_
             carrera = Console.ReadLine();
 
             Console.WriteLine($"\n{nombre}, bienvenido al curso de FA de {carrera}");
-
         }
-    }
+    }   static void ejer2()
+        { 
+            Console.WriteLine("\"Analia\"");
+        }
 }
--- SEMANA1_PY/SEMANA1_PY.py
+++ SEMANA1_PY/SEMANA1_PY.py
@@ -4,4 +4,7 @@ def ejer1():
 
     print(f"\n{nombre}, bienvenido al curso de FA de {carrera}")
 
-ejer1()
+def ejer2():
+    print("\"Analia\"")
+
+ejer2()
